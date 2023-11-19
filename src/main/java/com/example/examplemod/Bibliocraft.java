72d86c08c5cac51899b0fedd01ff3cc6852b5a63
IchHabeHunger54 <kingdavidaut@gmail.com>
2023-11-19 19:20:50 +0100

    edit metadata and main class to include the Bibliocraft name

---
+++ src/main/java/com/example/examplemod/Bibliocraft.java
@@ -0,0 +1,14 @@
+package com.example.examplemod;
+
+import net.neoforged.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod(Bibliocraft.MOD_ID)
+public class Bibliocraft {
+    public static final String MOD_ID = "bibliocraft";
+    public static final Logger LOGGER = LogManager.getLogger();
+
+    public Bibliocraft() {
+    }
+}
