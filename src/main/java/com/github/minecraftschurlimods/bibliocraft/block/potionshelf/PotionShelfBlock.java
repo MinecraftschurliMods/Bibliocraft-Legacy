b18ea436c728015ad84800454f3d6fc54a09720b
IchHabeHunger54 <kingdavidaut@gmail.com>
2023-12-04 17:37:54 +0100

    add right-click to insert functionality to potion shelf

--- src/main/java/com/github/minecraftschurlimods/bibliocraft/block/potionshelf/PotionShelfBlock.java
+++ src/main/java/com/github/minecraftschurlimods/bibliocraft/block/potionshelf/PotionShelfBlock.java
@@ -1,11 +1,16 @@
 package com.github.minecraftschurlimods.bibliocraft.block.potionshelf;
 
 import com.github.minecraftschurlimods.bibliocraft.block.BCBlock;
+import com.github.minecraftschurlimods.bibliocraft.init.BCTags;
 import com.github.minecraftschurlimods.bibliocraft.util.ShapeUtil;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.MenuProvider;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Rotation;
@@ -15,6 +20,7 @@ import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
+import net.neoforged.neoforge.network.NetworkHooks;
 import org.jetbrains.annotations.Nullable;
 
 @SuppressWarnings("deprecation")
@@ -45,5 +51,49 @@ public class PotionShelfBlock extends BCBlock {
             case SOUTH -> SOUTH_SHAPE;
             case WEST -> WEST_SHAPE;
         };
+    }
+
+    @Override
+    public InteractionResult use(BlockState state, Level level, BlockPos pos, Player player, InteractionHand hand, BlockHitResult hit) {
+        if (player.isSecondaryUseActive()) {
+            if (level.isClientSide()) return InteractionResult.SUCCESS;
+            NetworkHooks.openScreen((ServerPlayer) player, (MenuProvider) level.getBlockEntity(pos), pos);
+            return InteractionResult.SUCCESS;
+        }
+        Direction direction = state.getValue(FACING);
+        if (hit.getDirection() != direction && hit.getDirection() != direction.getOpposite())
+            return super.use(state, level, pos, player, hand, hit);
+        ItemStack stack = player.getItemInHand(hand);
+        if (!stack.isEmpty() && !stack.is(BCTags.Items.POTION_SHELF_POTIONS))
+            return super.use(state, level, pos, player, hand, hit);
+        int slot = lookingAtSlot(state, hit);
+        if (slot != -1) {
+            BlockEntity blockEntity = level.getBlockEntity(pos);
+            if (blockEntity instanceof PotionShelfBlockEntity potionShelf) {
+                ItemStack slotStack = potionShelf.getItem(slot);
+                if (!stack.isEmpty() || !slotStack.isEmpty()) {
+                    potionShelf.setItem(slot, stack);
+                    player.setItemInHand(hand, slotStack);
+                    return InteractionResult.SUCCESS;
+                }
+            }
+        }
+        return super.use(state, level, pos, player, hand, hit);
+    }
+
+    private static int lookingAtSlot(BlockState state, BlockHitResult hit) { // constant double values computed from PotionShelfBER#render
+        Direction.Axis axis = state.getValue(FACING).getClockWise().getAxis();
+        double hitX = 1.125 - (hit.getLocation().get(axis) - hit.getBlockPos().get(axis));
+        double hitY = hit.getLocation().y - hit.getBlockPos().getY();
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 4; j++) {
+                double minX = 0.171875 + j * 0.21875;
+                double maxX = minX + 0.21875;
+                double maxY = 0.90625 - i * 0.3125;
+                double minY = maxY - 0.21875;
+                if (hitX >= minX && hitX < maxX && hitY >= minY && hitY < maxY) return i * 4 + j;
+            }
+        }
+        return -1;
     }
 }
