3cba0955b3fd7a2d658c44966f1c43c0db8aca29
IchHabeHunger54 <kingdavidaut@gmail.com>
2023-12-27 02:42:02 +0100

    cleanup the recipe datagen a bit

--- src/main/java/com/github/minecraftschurlimods/bibliocraft/datagen/data/BCRecipeProvider.java
+++ src/main/java/com/github/minecraftschurlimods/bibliocraft/datagen/data/BCRecipeProvider.java
@@ -1,6 +1,7 @@
 package com.github.minecraftschurlimods.bibliocraft.datagen.data;
 
 import com.github.minecraftschurlimods.bibliocraft.init.BCItems;
+import com.github.minecraftschurlimods.bibliocraft.util.WoodTypeDeferredHolder;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.BlockFamilies;
 import net.minecraft.data.BlockFamily;
@@ -9,11 +10,8 @@ import net.minecraft.data.recipes.RecipeCategory;
 import net.minecraft.data.recipes.RecipeOutput;
 import net.minecraft.data.recipes.RecipeProvider;
 import net.minecraft.data.recipes.ShapedRecipeBuilder;
-import net.minecraft.world.item.BlockItem;
-import net.minecraft.world.item.DoubleHighBlockItem;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.Items;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.properties.WoodType;
 import net.neoforged.neoforge.common.Tags;
 import net.neoforged.neoforge.registries.DeferredHolder;
@@ -21,75 +19,55 @@ import org.jetbrains.annotations.Nullable;
 
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
+import java.util.function.BiFunction;
 
 public final class BCRecipeProvider extends RecipeProvider {
+    //Defining this here so the helpers can use it
+    private RecipeOutput output;
+
     public BCRecipeProvider(PackOutput output, CompletableFuture<HolderLookup.Provider> lookupProvider) {
         super(output, lookupProvider);
     }
 
     @Override
     protected void buildRecipes(RecipeOutput output) {
-        for (Map.Entry<WoodType, DeferredHolder<Item, BlockItem>> entry : BCItems.BOOKCASE.map().entrySet()) {
-            BlockFamily woodFamily = woodFamily(entry.getKey());
-            ShapedRecipeBuilder.shaped(RecipeCategory.BUILDING_BLOCKS, entry.getValue().get())
-                    .pattern("PSP")
-                    .pattern("PSP")
-                    .pattern("PSP")
-                    .define('P', woodFamily.getBaseBlock())
-                    .define('S', woodFamily.get(BlockFamily.Variant.SLAB))
-                    .unlockedBy("has_slab", has(woodFamily.get(BlockFamily.Variant.SLAB)))
-                    .save(output);
-        }
-        for (Map.Entry<WoodType, DeferredHolder<Item, DoubleHighBlockItem>> entry : BCItems.FANCY_ARMOR_STAND.map().entrySet()) {
-            BlockFamily woodFamily = woodFamily(entry.getKey());
-            ShapedRecipeBuilder.shaped(RecipeCategory.BUILDING_BLOCKS, entry.getValue().get())
-                    .pattern(" R ")
-                    .pattern(" R ")
-                    .pattern("SSS")
-                    .define('S', woodFamily.get(BlockFamily.Variant.SLAB))
-                    .define('R', Tags.Items.RODS_WOODEN)
-                    .unlockedBy("has_slab", has(woodFamily.get(BlockFamily.Variant.SLAB)))
-                    .save(output);
-        }
-        for (Map.Entry<WoodType, DeferredHolder<Item, BlockItem>> entry : BCItems.POTION_SHELF.map().entrySet()) {
-            BlockFamily woodFamily = woodFamily(entry.getKey());
-            ShapedRecipeBuilder.shaped(RecipeCategory.BUILDING_BLOCKS, entry.getValue().get())
-                    .pattern("SSS")
-                    .pattern("PBP")
-                    .pattern("SSS")
-                    .define('S', woodFamily.get(BlockFamily.Variant.SLAB))
-                    .define('P', woodFamily.getBaseBlock())
-                    .define('B', Items.GLASS_BOTTLE)
-                    .unlockedBy("has_glass_bottle", has(Items.GLASS_BOTTLE))
-                    .save(output);
-        }
-        for (Map.Entry<WoodType, DeferredHolder<Item, BlockItem>> entry : BCItems.SHELF.map().entrySet()) {
-            BlockFamily woodFamily = woodFamily(entry.getKey());
-            ShapedRecipeBuilder.shaped(RecipeCategory.BUILDING_BLOCKS, entry.getValue().get())
-                    .pattern("SSS")
-                    .pattern(" P ")
-                    .pattern("SSS")
-                    .define('P', woodFamily.getBaseBlock())
-                    .define('S', woodFamily.get(BlockFamily.Variant.SLAB))
-                    .unlockedBy("has_slab", has(woodFamily.get(BlockFamily.Variant.SLAB)))
-                    .save(output);
-        }
-        for (Map.Entry<WoodType, DeferredHolder<Item, BlockItem>> entry : BCItems.TOOL_RACK.map().entrySet()) {
-            BlockFamily woodFamily = woodFamily(entry.getKey());
-            ShapedRecipeBuilder.shaped(RecipeCategory.BUILDING_BLOCKS, entry.getValue().get())
-                    .pattern("SSS")
-                    .pattern("SIS")
-                    .pattern("SSS")
-                    .define('S', woodFamily.get(BlockFamily.Variant.SLAB))
-                    .define('I', Tags.Items.INGOTS_IRON)
-                    .unlockedBy("has_iron_ingot", has(Tags.Items.INGOTS_IRON))
-                    .save(output);
-        }
+        this.output = output;
+        forEachWoodType(BCItems.BOOKCASE, (builder, family) -> builder
+                .pattern("PSP")
+                .pattern("PSP")
+                .pattern("PSP")
+                .define('P', family.getBaseBlock())
+                .define('S', family.get(BlockFamily.Variant.SLAB)));
+        forEachWoodType(BCItems.FANCY_ARMOR_STAND, (builder, family) -> builder
+                .pattern(" R ")
+                .pattern(" R ")
+                .pattern("SSS")
+                .define('S', family.get(BlockFamily.Variant.SLAB))
+                .define('R', Tags.Items.RODS_WOODEN));
+        forEachWoodType(BCItems.POTION_SHELF, (builder, family) -> builder
+                .pattern("SSS")
+                .pattern("PBP")
+                .pattern("SSS")
+                .define('S', family.get(BlockFamily.Variant.SLAB))
+                .define('P', family.getBaseBlock())
+                .define('B', Items.GLASS_BOTTLE));
+        forEachWoodType(BCItems.SHELF, (builder, family) -> builder
+                .pattern("SSS")
+                .pattern(" P ")
+                .pattern("SSS")
+                .define('P', family.getBaseBlock())
+                .define('S', family.get(BlockFamily.Variant.SLAB)));
+        forEachWoodType(BCItems.TOOL_RACK, (builder, family) -> builder
+                .pattern("SSS")
+                .pattern("SIS")
+                .pattern("SSS")
+                .define('S', family.get(BlockFamily.Variant.SLAB))
+                .define('I', Tags.Items.INGOTS_IRON));
         ShapedRecipeBuilder.shaped(RecipeCategory.BUILDING_BLOCKS, BCItems.IRON_FANCY_ARMOR_STAND.get())
                 .pattern(" R ")
                 .pattern(" R ")
                 .pattern("SSS")
-                .define('S', Blocks.SMOOTH_STONE_SLAB)
+                .define('S', Items.SMOOTH_STONE_SLAB)
                 .define('R', Tags.Items.INGOTS_IRON)
                 .unlockedBy("has_iron_ingot", has(Tags.Items.INGOTS_IRON))
                 .save(output);
@@ -113,5 +91,17 @@ public final class BCRecipeProvider extends RecipeProvider {
         if (wood == WoodType.BAMBOO) return BlockFamilies.BAMBOO_PLANKS;
         if (wood == WoodType.CHERRY) return BlockFamilies.CHERRY_PLANKS;
         return null;
+    }
+
+    /**
+     * @param holder            The {@link WoodTypeDeferredHolder} to generate the recipes for.
+     * @param recipeTransformer The function to generate the recipe with.
+     */
+    private void forEachWoodType(WoodTypeDeferredHolder<Item, ? extends Item> holder, BiFunction<ShapedRecipeBuilder, BlockFamily, ShapedRecipeBuilder> recipeTransformer) {
+        for (Map.Entry<WoodType, ? extends DeferredHolder<Item, ? extends Item>> entry : holder.map().entrySet()) {
+            BlockFamily family = woodFamily(entry.getKey());
+            if (family == null) throw new RuntimeException("Tried to generate a recipe with an unknown wood type");
+            recipeTransformer.apply(ShapedRecipeBuilder.shaped(RecipeCategory.BUILDING_BLOCKS, entry.getValue().get()), family).unlockedBy("has_planks", has(family.getBaseBlock())).save(output);
+        }
     }
 }
