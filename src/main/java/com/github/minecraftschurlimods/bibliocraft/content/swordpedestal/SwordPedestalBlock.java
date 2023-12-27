7f2ca46c5575bb9bb86c00852d9b5251e0913b2b
IchHabeHunger54 <kingdavidaut@gmail.com>
2023-12-27 20:54:51 +0100

    some minor model optimizations

--- src/main/java/com/github/minecraftschurlimods/bibliocraft/content/swordpedestal/SwordPedestalBlock.java
+++ src/main/java/com/github/minecraftschurlimods/bibliocraft/content/swordpedestal/SwordPedestalBlock.java
@@ -24,22 +24,10 @@ import org.jetbrains.annotations.Nullable;
 @SuppressWarnings("deprecation")
 public class SwordPedestalBlock extends BCInteractibleBlock {
     private static final VoxelShape Z_SHAPE = ShapeUtil.combine(
-            Shapes.box(0.9375, 0, 0.25, 1, 0.0625, 0.75),
-            Shapes.box(0.875, 0, 0.25, 0.9375, 0.0625, 0.75),
-            Shapes.box(0.8125, 0, 0.25, 0.875, 0.125, 0.75),
-            Shapes.box(0.875, 0.0625, 0.25, 0.9375, 0.125, 0.75),
-            Shapes.box(0.8125, 0.125, 0.25, 0.875, 0.1875, 0.75),
-            Shapes.box(0.125, 0, 0.25, 0.1875, 0.125, 0.75),
-            Shapes.box(0.0625, 0, 0.25, 0.125, 0.0625, 0.75),
-            Shapes.box(0, 0, 0.25, 0.0625, 0.0625, 0.75),
-            Shapes.box(0.0625, 0.0625, 0.25, 0.125, 0.125, 0.75),
-            Shapes.box(0.125, 0.125, 0.25, 0.1875, 0.1875, 0.75),
-            Shapes.box(0.75, 0, 0.25, 0.8125, 0.1875, 0.75),
-            Shapes.box(0.75, 0.1875, 0.25, 0.8125, 0.25, 0.75),
-            Shapes.box(0.1875, 0.1875, 0.25, 0.25, 0.25, 0.75),
-            Shapes.box(0.1875, 0, 0.25, 0.25, 0.1875, 0.75),
-            Shapes.box(0.25, 0, 0.25, 0.75, 0.25, 0.75),
-            Shapes.box(0.25, 0, 0.25, 0.75, 0.25, 0.75));
+            Shapes.box(0, 0, 0.25, 1, 0.0625, 0.75),
+            Shapes.box(0.0625, 0.0625, 0.25, 0.9375, 0.125, 0.75),
+            Shapes.box(0.125, 0.125, 0.25, 0.875, 0.1875, 0.75),
+            Shapes.box(0.1875, 0.1875, 0.25, 0.8125, 0.25, 0.75));
     private static final VoxelShape X_SHAPE = ShapeUtil.rotate(Z_SHAPE, Rotation.CLOCKWISE_90);
 
     public SwordPedestalBlock(Properties properties) {
@@ -76,16 +64,16 @@ public class SwordPedestalBlock extends BCInteractibleBlock {
     }
 
     @Override
-    protected MapCodec<SwordPedestalBlock> codec() {
-        return simpleCodec(SwordPedestalBlock::new);
-    }
-
-    @Override
     public ItemStack getCloneItemStack(BlockState state, HitResult target, LevelReader level, BlockPos pos, Player player) {
         ItemStack stack = super.getCloneItemStack(state, target, level, pos, player);
         if (level.getBlockEntity(pos) instanceof SwordPedestalBlockEntity spbe) {
             SwordPedestalItem.setNBTColor(stack, spbe.getColor());
         }
         return stack;
+    }
+
+    @Override
+    protected MapCodec<SwordPedestalBlock> codec() {
+        return simpleCodec(SwordPedestalBlock::new);
     }
 }
