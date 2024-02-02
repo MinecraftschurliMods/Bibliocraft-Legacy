a9d6fcd05b5dc7621d304dcdd4012cc47c14d6f3
IchHabeHunger54 <kingdavidaut@gmail.com>
2024-02-02 17:42:19 +0100

    add missing mineable tags

--- src/main/java/com/github/minecraftschurlimods/bibliocraft/datagen/data/BCBlockTagsProvider.java
+++ src/main/java/com/github/minecraftschurlimods/bibliocraft/datagen/data/BCBlockTagsProvider.java
@@ -22,7 +22,7 @@ public class BCBlockTagsProvider extends BlockTagsProvider {
     protected void addTags(HolderLookup.Provider lookupProvider) {
         BibliocraftDatagenHelper.get().generateBlockTags(this::tag);
         tag(BCTags.Blocks.FANCY_ARMOR_STANDS).addTag(BCTags.Blocks.FANCY_ARMOR_STANDS_WOOD).add(BCBlocks.IRON_FANCY_ARMOR_STAND.get());
-        tag(BlockTags.MINEABLE_WITH_AXE).addTags(BCTags.Blocks.BOOKCASES, BCTags.Blocks.DISPLAY_CASES, BCTags.Blocks.FANCY_ARMOR_STANDS_WOOD, BCTags.Blocks.POTION_SHELVES, BCTags.Blocks.SHELVES, BCTags.Blocks.TOOL_RACKS);
+        tag(BlockTags.MINEABLE_WITH_AXE).addTags(BCTags.Blocks.BOOKCASES, BCTags.Blocks.DISPLAY_CASES, BCTags.Blocks.FANCY_ARMOR_STANDS_WOOD, BCTags.Blocks.LABELS, BCTags.Blocks.POTION_SHELVES, BCTags.Blocks.SEATS, BCTags.Blocks.SHELVES, BCTags.Blocks.TOOL_RACKS);
         tag(BlockTags.MINEABLE_WITH_PICKAXE).add(BCBlocks.IRON_FANCY_ARMOR_STAND.get(), BCBlocks.SWORD_PEDESTAL.get());
     }
 }
