f272732d480e6f373b52fe859451a963f991be6e
Minecraftschurli <minecraftschurli@gmail.com>
2024-03-10 23:13:21 +0100

    use common build system

---
+++ build.gradle.kts
@@ -0,0 +1,37 @@
+plugins {
+    idea
+    id("com.github.minecraftschurlimods.helperplugin")
+}
+
+dependencies {
+    implementation(helper.neoforge())
+}
+
+helper.withCommonRuns()
+helper.withDataGenRuns()
+
+helper.publication.pom {
+    organization {
+        name = "Minecraftschurli Mods"
+        url = "https://github.com/MinecraftschurliMods"
+    }
+    developers {
+        developer {
+            id = "minecraftschurli"
+            name = "Minecraftschurli"
+            email = "minecraftschurli@gmail.com"
+            url = "https://github.com/Minecraftschurli"
+            organization = "Minecraftschurli Mods"
+            organizationUrl = "https://github.com/MinecraftschurliMods"
+            timezone = "Europe/Vienna"
+        }
+        developer {
+            id = "ichhabehunger54"
+            name = "IchHabeHunger54"
+            url = "https://github.com/IchHabeHunger54"
+            organization = "Minecraftschurli Mods"
+            organizationUrl = "https://github.com/MinecraftschurliMods"
+            timezone = "Europe/Vienna"
+        }
+    }
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    plugins {
+        id("net.neoforged.gradle.userdev") version "7.0.80"
+        id("com.github.minecraftschurlimods.helperplugin") version "1.9"
+    }
+    repositories {
+        mavenLocal()
+        gradlePluginPortal()
+        maven { url = uri("https://maven.neoforged.net/releases") }
+        maven { url = uri("https://minecraftschurli.ddns.net/repository/maven-public") }
+    }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
+}
+
+rootProject.name = "Bibliocraft-Legacy"
