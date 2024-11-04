287eb3d5c380e296e598313012abf8b80b8f5ec0
Minecraftschurli <minecraftschurli@gmail.com>
2024-11-04 11:01:06 +0100

    move back to helper plugin

---
+++ build.gradle.kts
@@ -0,0 +1,87 @@
+import com.github.minecraftschurlimods.helperplugin.api
+import com.github.minecraftschurlimods.helperplugin.version
+
+plugins {
+    idea
+    id("net.neoforged.gradle.userdev")
+    id("com.github.minecraftschurlimods.helperplugin")
+}
+
+helper.withApiSourceSet()
+helper.withDataGenSourceSet()
+helper.withTestSourceSet()
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+    maven {
+        name = "blamejared Maven"
+        url = uri("https://maven.blamejared.com")
+        content {
+            includeGroup("mezz.jei")
+        }
+    }
+}
+
+val jei = helper.dependencies.jei()
+
+dependencies {
+    implementation(helper.neoforge())
+    testImplementation(helper.testframework())
+
+    // jei for integration
+    val jeiApiDep = helper.minecraftVersion.zip(jei.version) { mc, version -> "mezz.jei:jei-${mc}-common-api:${version}" }
+    val jeiDep = helper.minecraftVersion.zip(jei.version) { mc, version -> "mezz.jei:jei-${mc}-neoforge:${version}" }
+    compileOnly(jeiApiDep)
+    if (!helper.runningInCI.getOrElse(false)) {
+        runtimeOnly(jeiDep)
+    }
+
+    testImplementation("org.junit.jupiter:junit-jupiter:${project.properties["junit_version"]}")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    sourceSets.forEach {
+        it.compileOnlyConfigurationName("org.jetbrains:annotations:23.0.0")
+    }
+}
+
+helper.withCommonRuns()
+helper.withGameTestRuns()
+helper.withDataGenRuns()
+
+minecraft.accessTransformers.file("src/main/resources/META-INF/accesstransformer.cfg")
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.javadoc {
+    classpath = sourceSets.api.get().compileClasspath
+    source = sourceSets.api.get().allJava
+}
+
+helper.publication.pom {
+    organization {
+        name = "Minecraftschurli Mods"
+        url = "https://github.com/MinecraftschurliMods"
+    }
+    developers {
+        developer {
+            id = "minecraftschurli"
+            name = "Minecraftschurli"
+            email = "minecraftschurli@gmail.com"
+            url = "https://github.com/Minecraftschurli"
+            organization = "Minecraftschurli Mods"
+            organizationUrl = "https://github.com/MinecraftschurliMods"
+            timezone = "Europe/Vienna"
+        }
+        developer {
+            id = "ichhabehunger54"
+            name = "IchHabeHunger54"
+            url = "https://github.com/IchHabeHunger54"
+            organization = "Minecraftschurli Mods"
+            organizationUrl = "https://github.com/MinecraftschurliMods"
+            timezone = "Europe/Vienna"
+        }
+    }
+}
