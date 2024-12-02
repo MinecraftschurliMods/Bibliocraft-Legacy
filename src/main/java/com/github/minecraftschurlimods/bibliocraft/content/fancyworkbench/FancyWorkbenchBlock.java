3de853bea73bfc89c7a62c8289fa255293ed8b72
IchHabeHunger54 <kingdavidaut@gmail.com>
2024-12-02 22:25:04 +0100

    add voxel shapes

--- src/main/java/com/github/minecraftschurlimods/bibliocraft/content/fancyworkbench/FancyWorkbenchBlock.java
+++ src/main/java/com/github/minecraftschurlimods/bibliocraft/content/fancyworkbench/FancyWorkbenchBlock.java
@@ -1,13 +1,16 @@
 package com.github.minecraftschurlimods.bibliocraft.content.fancyworkbench;
 
 import com.github.minecraftschurlimods.bibliocraft.init.BCBlockEntities;
+import com.github.minecraftschurlimods.bibliocraft.util.ShapeUtil;
 import com.github.minecraftschurlimods.bibliocraft.util.content.BCFacingEntityBlock;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Rotation;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.entity.BlockEntityTicker;
 import net.minecraft.world.level.block.entity.BlockEntityType;
@@ -15,13 +18,27 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.Objects;
 
 public class FancyWorkbenchBlock extends BCFacingEntityBlock {
     public static final BooleanProperty POWERED = BlockStateProperties.POWERED;
-    
+    private static final VoxelShape NORTH_SHAPE = ShapeUtil.combine(
+            Shapes.box(0, 0, 0, 1, 0.25, 1),
+            Shapes.box(0, 0.25, 0.0625, 1, 0.625, 1),
+            Shapes.box(0, 0.25, 0, 0.125, 0.625, 0.0625),
+            Shapes.box(0.875, 0.25, 0, 1, 0.625, 0.0625),
+            Shapes.box(0, 0.625, 0, 1, 0.875, 1),
+            Shapes.box(0.0625, 0.875, 0.0625, 0.9375, 0.9375, 0.9375),
+            Shapes.box(0, 0.9375, 0, 1, 1, 1));
+    private static final VoxelShape EAST_SHAPE = ShapeUtil.rotate(NORTH_SHAPE, Rotation.CLOCKWISE_90);
+    private static final VoxelShape SOUTH_SHAPE = ShapeUtil.rotate(NORTH_SHAPE, Rotation.CLOCKWISE_180);
+    private static final VoxelShape WEST_SHAPE = ShapeUtil.rotate(NORTH_SHAPE, Rotation.COUNTERCLOCKWISE_90);
+
     public FancyWorkbenchBlock(Properties properties) {
         super(properties);
         registerDefaultState(defaultBlockState().setValue(POWERED, false));
@@ -37,6 +54,16 @@ public class FancyWorkbenchBlock extends BCFacingEntityBlock {
     @Nullable
     public BlockEntity newBlockEntity(BlockPos pos, BlockState state) {
         return new FancyWorkbenchBlockEntity(pos, state);
+    }
+
+    @Override
+    public VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
+        return switch (state.getValue(FACING)) {
+            default -> NORTH_SHAPE;
+            case EAST -> EAST_SHAPE;
+            case SOUTH -> SOUTH_SHAPE;
+            case WEST -> WEST_SHAPE;
+        };
     }
 
     @Override
