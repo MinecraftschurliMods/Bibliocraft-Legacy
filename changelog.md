9ed5269183e1c87162a365609ae2756b1e9e3ba3
IchHabeHunger54 <kingdavidaut@gmail.com>
2025-01-06 17:56:00 +0100

    fix a dupe issue with the slotted book

--- changelog.md
+++ changelog.md
@@ -1,10 +1,3 @@
-# Additions
+# Fixes
 
-- New fancy clock and grandfather clock blocks
-- Sounds for the tape measure
-- Config to allow removing the various wooden and colored variants from JEI
-
-# Changes
-
-- Bookcases now have enchantment power depending on the amount of books in them
-- Redstone: Volume 1 was renamed to Redstone Book
+- Fix a dupe issue with the slotted book
--- src/main/java/com/github/minecraftschurlimods/bibliocraft/content/slottedbook/SlottedBookMenu.java
+++ src/main/java/com/github/minecraftschurlimods/bibliocraft/content/slottedbook/SlottedBookMenu.java
@@ -1,5 +1,6 @@
 package com.github.minecraftschurlimods.bibliocraft.content.slottedbook;
 
+import com.github.minecraftschurlimods.bibliocraft.init.BCItems;
 import com.github.minecraftschurlimods.bibliocraft.init.BCMenus;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.world.Container;
@@ -17,11 +18,20 @@ public class SlottedBookMenu extends AbstractContainerMenu {
         addSlot(new SlottedBookSlot(container, 0, 80, 34));
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 9; j++) {
-                addSlot(new Slot(inventory, i * 9 + j + 9, 8 + j * 18, 141 + i * 18));
+                int slot = i * 9 + j + 9;
+                if (inventory.getItem(slot).is(BCItems.SLOTTED_BOOK.get())) {
+                    addSlot(new ReadOnlySlot(inventory, slot, 8 + j * 18, 141 + i * 18));
+                } else {
+                    addSlot(new Slot(inventory, slot, 8 + j * 18, 141 + i * 18));
+                }
             }
         }
         for (int i = 0; i < 9; i++) {
-            addSlot(new Slot(inventory, i, 8 + i * 18, 199));
+            if (inventory.getItem(i).is(BCItems.SLOTTED_BOOK.get())) {
+                addSlot(new ReadOnlySlot(inventory, i, 8 + i * 18, 199));
+            } else {
+                addSlot(new Slot(inventory, i, 8 + i * 18, 199));
+            }
         }
     }
 
@@ -66,5 +76,21 @@ public class SlottedBookMenu extends AbstractContainerMenu {
     @Override
     public boolean stillValid(Player player) {
         return true;
+    }
+
+    private static class ReadOnlySlot extends Slot {
+        public ReadOnlySlot(Container container, int slot, int x, int y) {
+            super(container, slot, x, y);
+        }
+
+        @Override
+        public boolean mayPickup(Player player) {
+            return false;
+        }
+
+        @Override
+        public boolean mayPlace(ItemStack stack) {
+            return false;
+        }
     }
 }
