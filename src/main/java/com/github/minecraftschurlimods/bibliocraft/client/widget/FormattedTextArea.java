1461b75b2410e2735cec99cede03f30a03a4c852
IchHabeHunger54 <kingdavidaut@gmail.com>
2025-03-10 13:32:01 +0100

    fancy sign gui: text selection

--- src/main/java/com/github/minecraftschurlimods/bibliocraft/client/widget/FormattedTextArea.java
+++ src/main/java/com/github/minecraftschurlimods/bibliocraft/client/widget/FormattedTextArea.java
@@ -32,6 +32,7 @@ public class FormattedTextArea extends AbstractWidget {
     private final List<FormattedLine> lines;
     private int cursorX = 0;
     private int cursorY = 0;
+    private int highlightX = 0;
     private long focusedTimestamp = Util.getMillis();
     private Consumer<FormattedLine> onLineChange;
 
@@ -84,6 +85,15 @@ public class FormattedTextArea extends AbstractWidget {
         } else {
             drawText(graphics, format(text, style), x + 1, y, color, size, mode);
         }
+        
+        // draw highlight
+        if (cursorInLine && cursorX != highlightX) {
+            int min = Math.min(cursorX, highlightX);
+            int max = Math.max(cursorX, highlightX);
+            int minWidth = font.width(format(text.substring(0, min), style));
+            int maxWidth = font.width(format(text.substring(0, max), style));
+            graphics.fill(RenderType.guiTextHighlight(), x + (int) (minWidth * scale), y - 1, x + (int) (maxWidth * scale), (int) (y + 9 * scale + 1), 0xff0000ff);
+        }
     }
 
     private int drawText(GuiGraphics graphics, FormattedCharSequence text, float x, float y, int color, int size, FormattedLine.Mode mode) {
@@ -119,61 +129,69 @@ public class FormattedTextArea extends AbstractWidget {
     @Override
     public boolean keyPressed(int keyCode, int scanCode, int modifiers) {
         if (!isActive() || !isFocused()) return false;
+        FormattedLine line = lines.get(cursorY);
+        String text = line.text();
         switch (keyCode) {
-            case GLFW.GLFW_KEY_DOWN: //TODO selection
-                if (cursorY < lines.size() - 1) {
-                    cursorX = getCursorXForNewLine(cursorY, cursorY + 1);
-                    cursorY++;
-                    onLineChange.accept(lines.get(cursorY));
+            case GLFW.GLFW_KEY_DOWN:
+                if (Screen.hasShiftDown()) {
+                    moveCursor(text.length(), cursorY, true);
+                } else if (cursorY < lines.size() - 1) {
+                    moveCursor(getCursorXForNewLine(cursorY, cursorY + 1), cursorY + 1, false);
                 }
                 return true;
-            case GLFW.GLFW_KEY_UP: //TODO selection
-                if (cursorY > 0) {
-                    cursorX = getCursorXForNewLine(cursorY, cursorY - 1);
-                    cursorY--;
-                    onLineChange.accept(lines.get(cursorY));
+            case GLFW.GLFW_KEY_UP:
+                if (Screen.hasShiftDown()) {
+                    moveCursor(0, cursorY, true);
+                } else if (cursorY > 0) {
+                    moveCursor(getCursorXForNewLine(cursorY, cursorY - 1), cursorY - 1, false);
                 }
                 return true;
-            case GLFW.GLFW_KEY_LEFT: //TODO selection
-                cursorX = Screen.hasControlDown() ? getWordPosition(-1) : Math.max(0, cursorX - 1);
+            case GLFW.GLFW_KEY_LEFT:
+                moveCursor(Screen.hasControlDown() ? getWordPosition(-1) : Math.max(0, cursorX - 1), cursorY, Screen.hasShiftDown());
                 return true;
-            case GLFW.GLFW_KEY_RIGHT: //TODO selection
-                cursorX = Screen.hasControlDown() ? getWordPosition(1) : Math.min(cursorX + 1, lines.get(cursorY).text().length());
+            case GLFW.GLFW_KEY_RIGHT:
+                moveCursor(Screen.hasControlDown() ? getWordPosition(1) : Math.min(text.length(), cursorX + 1), cursorY, Screen.hasShiftDown());
                 return true;
             case GLFW.GLFW_KEY_BACKSPACE:
-                if (cursorX > 0) {
+                if (highlightX != cursorX) {
+                    deleteHighlight();
+                } else if (cursorX > 0) {
                     int x = Screen.hasControlDown() ? getWordPosition(-1) : cursorX - 1;
-                    lines.set(cursorY, lines.get(cursorY).withText(lines.get(cursorY).text().substring(0, x) + lines.get(cursorY).text().substring(cursorX)));
+                    lines.set(cursorY, line.withText(text.substring(0, x) + text.substring(cursorX)));
                     cursorX = x;
                 }
                 return true;
             case GLFW.GLFW_KEY_DELETE:
-                if (cursorX < lines.size() - 1) {
+                if (highlightX != cursorX) {
+                    deleteHighlight();
+                } else if (cursorX < lines.size() - 1) {
                     int x = Screen.hasControlDown() ? getWordPosition(1) : cursorX + 1;
-                    lines.set(cursorY, lines.get(cursorY).withText(lines.get(cursorY).text().substring(0, cursorX) + lines.get(cursorY).text().substring(x)));
+                    lines.set(cursorY, line.withText(text.substring(0, cursorX) + text.substring(x)));
                 }
                 return true;
-            case GLFW.GLFW_KEY_HOME: //TODO selection
-                cursorX = 0;
+            case GLFW.GLFW_KEY_HOME:
+                moveCursor(0, cursorY, Screen.hasShiftDown());
                 return true;
-            case GLFW.GLFW_KEY_END: //TODO selection
-                cursorX = lines.get(cursorY).text().length();
+            case GLFW.GLFW_KEY_END:
+                moveCursor(text.length(), cursorY, Screen.hasShiftDown());
                 return true;
         }
         if (Screen.isSelectAll(keyCode)) {
-            //TODO
+            cursorX = 0;
+            highlightX = text.length();
             return true;
         }
         if (Screen.isCopy(keyCode)) {
-            //TODO
+            Minecraft.getInstance().keyboardHandler.setClipboard(text.substring(Math.min(cursorX, highlightX), Math.max(cursorX, highlightX)));
             return true;
         }
         if (Screen.isPaste(keyCode)) {
-            //TODO
+            insertText(Minecraft.getInstance().keyboardHandler.getClipboard());
             return true;
         }
         if (Screen.isCut(keyCode)) {
-            //TODO
+            Minecraft.getInstance().keyboardHandler.setClipboard(text.substring(Math.min(cursorX, highlightX), Math.max(cursorX, highlightX)));
+            deleteHighlight();
             return true;
         }
         return super.keyPressed(keyCode, scanCode, modifiers);
@@ -205,6 +223,18 @@ public class FormattedTextArea extends AbstractWidget {
             }
         }
         return x;
+    }
+
+    private void moveCursor(int x, int y, boolean highlight) {
+        int oldY = cursorY;
+        cursorX = x;
+        cursorY = y;
+        if (y != oldY) {
+            onLineChange.accept(lines.get(y));
+        }
+        if (!highlight) {
+            highlightX = cursorX;
+        }
     }
 
     @Override
@@ -283,11 +313,21 @@ public class FormattedTextArea extends AbstractWidget {
         return false;
     }
 
-    private boolean insertText(String text) {
+    private void deleteHighlight() {
+        FormattedLine line = lines.get(cursorY);
+        lines.set(cursorY, line.withText(line.text().substring(0, Math.min(highlightX, cursorX) + 1) + line.text().substring(Math.max(highlightX, cursorX))));
+        moveCursor(Math.min(highlightX, cursorX), cursorY, false);
+    }
+
+    private boolean insertText(String s) {
+        String text = StringUtil.filterText(s);
         FormattedLine line = lines.get(cursorY);
         String oldText = line.text();
         if (!tryEdit(
-                () -> lines.set(cursorY, line.withText(line.text().substring(0, cursorX) + text + line.text().substring(cursorX))),
+                () -> {
+                    deleteHighlight();
+                    lines.set(cursorY, line.withText(line.text().substring(0, cursorX) + text + line.text().substring(cursorX)));
+                },
                 () -> lines.set(cursorY, line.withText(oldText))
         )) return false;
         cursorX += text.length();
