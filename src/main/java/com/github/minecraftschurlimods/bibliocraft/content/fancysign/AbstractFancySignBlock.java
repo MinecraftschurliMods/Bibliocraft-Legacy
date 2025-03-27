1f457b66621a5b4311da5e8c406977ae86ad4ca7
IchHabeHunger54 <kingdavidaut@gmail.com>
2025-03-27 17:04:18 +0100

    optimize imports

--- src/main/java/com/github/minecraftschurlimods/bibliocraft/content/fancysign/AbstractFancySignBlock.java
+++ src/main/java/com/github/minecraftschurlimods/bibliocraft/content/fancysign/AbstractFancySignBlock.java
@@ -13,7 +13,6 @@ import net.minecraft.world.ItemInteractionResult;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.LevelEvent;
