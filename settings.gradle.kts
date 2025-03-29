b46124e4093613b3b26c235d0a464644b0ba8eb9
Minecraftschurli <minecraftschurli@gmail.com>
2025-03-29 12:03:13 +0100

    update helperplugin to fixed version

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,7 +1,7 @@
 pluginManagement {
     plugins {
         id("net.neoforged.gradle.userdev") version "7.0.171"
-        id("com.github.minecraftschurlimods.helperplugin") version "1.15"
+        id("com.github.minecraftschurlimods.helperplugin") version "1.17"
     }
     repositories {
         mavenLocal()
