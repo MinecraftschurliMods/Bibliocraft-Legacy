0cf5951b107030b0384463917a7aaccf43751850
IchHabeHunger54 <kingdavidaut@gmail.com>
2025-04-18 22:42:38 +0200

    big book cloning recipes

--- src/main/java/com/github/minecraftschurlimods/bibliocraft/content/bigbook/BigBookCloningRecipe.java
+++ src/main/java/com/github/minecraftschurlimods/bibliocraft/content/bigbook/BigBookCloningRecipe.java
@@ -1,7 +1,10 @@
 package com.github.minecraftschurlimods.bibliocraft.content.bigbook;
 
+import com.github.minecraftschurlimods.bibliocraft.init.BCDataComponents;
+import com.github.minecraftschurlimods.bibliocraft.init.BCItems;
 import com.github.minecraftschurlimods.bibliocraft.init.BCRecipeSerializers;
 import net.minecraft.core.HolderLookup;
+import net.minecraft.core.NonNullList;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.CraftingBookCategory;
 import net.minecraft.world.item.crafting.CraftingInput;
@@ -16,17 +19,61 @@ public class BigBookCloningRecipe extends CustomRecipe {
 
     @Override
     public boolean matches(CraftingInput input, Level level) {
-        return false;
+        int books = 0;
+        ItemStack stack = ItemStack.EMPTY;
+        for (int i = 0; i < input.size(); i++) {
+            ItemStack written = input.getItem(i);
+            if (written.isEmpty()) continue;
+            if (written.is(BCItems.WRITTEN_BIG_BOOK)) {
+                if (!stack.isEmpty()) return false;
+                stack = written;
+            } else if (written.is(BCItems.BIG_BOOK)) {
+                books++;
+            } else return false;
+        }
+        return !stack.isEmpty() && books > 0;
     }
 
     @Override
     public ItemStack assemble(CraftingInput input, HolderLookup.Provider registries) {
-        return null;
+        int books = 0;
+        ItemStack stack = ItemStack.EMPTY;
+        for (int i = 0; i < input.size(); i++) {
+            ItemStack written = input.getItem(i);
+            if (written.isEmpty()) continue;
+            if (written.is(BCItems.WRITTEN_BIG_BOOK)) {
+                if (!stack.isEmpty()) return ItemStack.EMPTY;
+                stack = written;
+            } else if (written.is(BCItems.BIG_BOOK)) {
+                books++;
+            } else return ItemStack.EMPTY;
+        }
+        WrittenBigBookContent content = stack.get(BCDataComponents.WRITTEN_BIG_BOOK_CONTENT);
+        if (stack.isEmpty() || books < 1 || content == null) return ItemStack.EMPTY;
+        WrittenBigBookContent contentCopy = content.tryCraftCopy();
+        if (contentCopy == null) return ItemStack.EMPTY;
+        ItemStack result = stack.copyWithCount(books);
+        result.set(BCDataComponents.WRITTEN_BIG_BOOK_CONTENT, contentCopy);
+        return result;
+    }
+
+    @Override
+    public NonNullList<ItemStack> getRemainingItems(CraftingInput input) {
+        NonNullList<ItemStack> list = NonNullList.withSize(input.size(), ItemStack.EMPTY);
+        for (int i = 0; i < list.size(); i++) {
+            ItemStack stack = input.getItem(i);
+            if (stack.hasCraftingRemainingItem()) {
+                list.set(i, stack.getCraftingRemainingItem());
+            } else if (stack.is(BCItems.WRITTEN_BIG_BOOK)) {
+                list.set(i, stack.copyWithCount(1));
+            }
+        }
+        return list;
     }
 
     @Override
     public boolean canCraftInDimensions(int width, int height) {
-        return false;
+        return width >= 3 && height >= 3;
     }
 
     @Override
--- src/main/java/com/github/minecraftschurlimods/bibliocraft/content/bigbook/WrittenBigBookContent.java
+++ src/main/java/com/github/minecraftschurlimods/bibliocraft/content/bigbook/WrittenBigBookContent.java
@@ -7,23 +7,30 @@ import net.minecraft.network.RegistryFriendlyByteBuf;
 import net.minecraft.network.codec.ByteBufCodecs;
 import net.minecraft.network.codec.StreamCodec;
 import net.minecraft.util.ExtraCodecs;
+import net.minecraft.world.item.component.WrittenBookContent;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 
 public record WrittenBigBookContent(List<List<FormattedLine>> pages, String title, String author, int generation, int currentPage) {
     public static final Codec<WrittenBigBookContent> CODEC = RecordCodecBuilder.create(inst -> inst.group(
             FormattedLine.CODEC.sizeLimitedListOf(256).listOf().fieldOf("pages").forGetter(WrittenBigBookContent::pages),
-            Codec.string(0, 32).fieldOf("title").forGetter(WrittenBigBookContent::title),
+            Codec.string(0, WrittenBookContent.TITLE_MAX_LENGTH).fieldOf("title").forGetter(WrittenBigBookContent::title),
             Codec.STRING.fieldOf("author").forGetter(WrittenBigBookContent::author),
-            ExtraCodecs.intRange(0, 3).optionalFieldOf("generation", 0).forGetter(WrittenBigBookContent::generation),
+            ExtraCodecs.intRange(0, WrittenBookContent.MAX_GENERATION).optionalFieldOf("generation", 0).forGetter(WrittenBigBookContent::generation),
             ExtraCodecs.intRange(0, 255).optionalFieldOf("current_page", 0).forGetter(WrittenBigBookContent::currentPage)
     ).apply(inst, WrittenBigBookContent::new));
     public static final StreamCodec<RegistryFriendlyByteBuf, WrittenBigBookContent> STREAM_CODEC = StreamCodec.composite(
             FormattedLine.STREAM_CODEC.apply(ByteBufCodecs.list()).apply(ByteBufCodecs.list()), WrittenBigBookContent::pages,
-            ByteBufCodecs.stringUtf8(32), WrittenBigBookContent::title,
+            ByteBufCodecs.stringUtf8(WrittenBookContent.TITLE_MAX_LENGTH), WrittenBigBookContent::title,
             ByteBufCodecs.STRING_UTF8, WrittenBigBookContent::author,
             ByteBufCodecs.VAR_INT, WrittenBigBookContent::generation,
             ByteBufCodecs.VAR_INT, WrittenBigBookContent::currentPage,
             WrittenBigBookContent::new);
     public static final WrittenBigBookContent DEFAULT = new WrittenBigBookContent(List.of(), "", "", 0, 0);
+
+    @Nullable
+    public WrittenBigBookContent tryCraftCopy() {
+        return generation >= WrittenBookContent.MAX_CRAFTABLE_GENERATION ? null : new WrittenBigBookContent(pages, title, author, generation + 1, currentPage);
+    }
 }
